141f0a6683062f1e90ec60c06e5f68bb408a7ae2
Joseph Luna <josephluna@Josephs-MBP.local>
2019-08-18 22:18:54 -0500

    almost complete added all required helper methods and endpoints working on true false

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,3 +1,37 @@
 const db = require('../data/db-config');
 
 
+const getProjects = () => {
+    return db('projects');
+}
+
+const addProject = (project) => {
+    return db('projects').insert({projectName:project.name, projectDescription:project.description, projectCompleted:project.completed});
+}
+
+const getResources = () => {
+    return db('resources');
+}
+
+const addResources = (resource) => {
+    return db('resources').insert({resourceName:resource.name, recourceDescription:resource.description});
+}
+
+const getTasks = () => {
+    return db('task')
+            .join('projects', 'projects.id', '=', 'task.projectID')
+            .select('taskDescription','taskNotes', 'taskCompleted', 'projectName', 'projectDescription', 'projectCompleted');
+}
+
+const addTask = (task) => {
+    return db('task').insert({taskDescription:task.description ,taskNotes:task.notes ,taskCompleted:task.completed, projectID:task.projectID});
+}
+
+module.exports = {
+    getProjects,
+    addProject,
+    getResources,
+    addResources,
+    getTasks,
+    addTask
+}
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -2,12 +2,78 @@ const express = require('express');
 const router = express.Router();
 const projectmodel = require('./projects-model');
 
+router.get('/', async (req,res) => {
+    
+    try{
+        const projects = await projectmodel.getProjects();
+        console.log(projects);
+        projects.projectCompleted === 0 ? projects.projectCompleted = false : projects.projectCompleted = true;
+        res.status(200).json(projects);
+    }
+    catch({message}){
+        res.status(500).json(message);
+    }
+});
+
+router.post('/', async (req, res) => {
+
+    try{
+        const addedProject = await projectmodel.addProject(req.body);
+        res.status(201).json(addedProject);
+    }
+    catch({message}){
+        res.status(500).json(message);
+    }
+});
 
 
 
+router.get('/resources', async (req,res) => {
+    
+    try{
+        const resources = await projectmodel.getResources();
+        res.status(200).json(resources);
+    }
+    catch({message}){
+        res.status(500).json(message);
+    }
+});
 
 
+router.post('/resources', async (req, res) => {
 
+    try{
+        const addedResources = await projectmodel.addResources(req.body);
+        res.status(201).json(addedResources);
+    }
+    catch({message}){
+        res.status(500).json(message);
+    }
+});
+
+
+router.get('/tasks', async (req,res) => {
+    
+    try{
+        const tasks = await projectmodel.getTasks();
+        res.status(200).json(tasks);
+    }
+    catch({message}){
+        res.status(500).json(message);
+    }
+});
+
+
+router.post('/tasks', async (req, res) => {
+
+    try{
+        const addedTasks = await projectmodel.addTask(req.body);
+        res.status(201).json(addedTasks);
+    }
+    catch({message}){
+        res.status(500).json(message);
+    }
+});
 
 
 
